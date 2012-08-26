2785c6e715b0ccfea629daacf656c689d43e7da3
Konzertheld <christian.gredig@googlemail.com>
2012-08-26 23:50:16 +0200

    Remove old code from the footer.

--- footer.php
+++ footer.php
@@ -1,14 +1,5 @@
 	<div id="stickyfooter">
-	<?php
-		if($page=="photos")
-		{
-			$newoffset = $offset+$maxitems;
-			print "<a href='?album=$album&number=$number&id=$id&offset=$newoffset' class='menuelement'>Next $maxitems</a>";
-			$newoffset = $offset-$maxitems;
-			if($newoffset>=0)
-				print "<a href='?album=$album&number=$number&id=$id&offset=$newoffset' class='menuelement'>Previous $maxitems</a>";
-		}
-	?>
+	
 	</div>
 </body>
 
