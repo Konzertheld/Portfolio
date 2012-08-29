861d31392d8794c0bf99e5edfbccf42ffd75e6bd
Konzertheld <christian.gredig@googlemail.com>
2012-08-29 23:37:33 +0200

    Add areas

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 	<div id="stickyfooter">
-	
+		<?php echo $theme->area('footer'); ?>
 	</div>
 </body>
 
--- header.php
+++ header.php
@@ -11,8 +11,8 @@
 <body>
 	<div id="wrapper">
 		<div id="header">
-			
+			<?php echo $theme->area('header'); ?>
 		</div>
 		<div id="menu">
-				
+				<?php echo $theme->area('menubar'); ?>
 		</div>
