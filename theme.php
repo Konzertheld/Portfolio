054f5ed92fb6705d6b036db8adb26d35c37fa0c9
Konzertheld <christian.gredig@gmail.com>
2013-03-15 23:09:58 +0100

    Content types should not be generated by themes. Instead, themes should integrate with plugins that provide content types. So get rid of all the content type code.

--- theme.php
+++ theme.php
@@ -4,65 +4,21 @@
  * by Konzertheld
  * http://konzertheld.de
  */
-
- // TODO: Deactivate content type on deactivation
  
 class Portfolio extends Theme
 {	
-	/**
-	 * Call initialization on theme activation.
-	 */
-	public function action_theme_activated()
-	{
-		Post::add_new_type( 'image' );
-		// Give anonymous users access
-		$group = UserGroup::get_by_name( 'anonymous' );
-		$group->grant( 'post_image', 'read');
-	}
-	
 	/**
 	 * Execute on theme init to apply these filters to output
 	 */
 	public function action_init_theme()
 	{
 		Format::apply('autop', 'comment_content_out');
-		$this->load_text_domain('TheViewInside');
 	}
-	
-	public function action_form_publish($form, $post, $context)
-	{
-		$form->insert('tags', 'text', 'thumbnail_url', 'null:null', _t('Thumbnail URL'), 'admincontrol_textArea');
-		$form->thumbnail_url->value = $post->info->thumbnail_url;
-		$form->thumbnail_url->template = 'admincontrol_text';
-		$form->insert('tags', 'text', 'fullsize_url', 'null:null', _t('Fullsize URL'), 'admincontrol_textArea');
-		$form->fullsize_url->value = $post->info->fullsize_url;
-		$form->fullsize_url->template = 'admincontrol_text';
-	}
-	
-	public function action_publish_post( $post, $form )
-	{
-		$post->info->thumbnail_url = $form->thumbnail_url->value;
-		$post->info->fullsize_url = $form->fullsize_url->value;
-	}
-	
-	public function action_admin_header($theme)
-	{
-
-	}
-	
+		
 	/**
 	 * Create theme options
 	 */
 	public function action_theme_ui( $theme )
-	{
-		
-	}
-	
-	/**
-	 * Redirect to the theme admin to force a full reload
-	 * Required for the generated text fields to update
-	 */
-	function options_callback($form)
 	{
 		
 	}
@@ -81,7 +37,7 @@ class Portfolio extends Theme
 
 	/**
 	 * Convert a post's tags ArrayObject into a usable list of links
-	 *
+	 * @todo this should be done with TermFormat now
 	 * @param array $array The Tags object from a Post object
 	 * @return string The HTML of the linked tags
 	 */
@@ -91,45 +47,5 @@ class Portfolio extends Theme
 		$out = '<ul>' . implode('', $array_out) . '</ul>';
 		return $out;
  	}
-	
-	public function filter_post_title_out($title, $post)
-	{
-		if(isset($post->info->thumbnail_url))
-		{
-			return "<img src='" . $post->info->thumbnail_url_out . "' alt='$title' title='$title'>";
-		}
-		else
-		{
-			return $title;
-		}
-	}
-	
-	public function filter_post_info_thumbnail_url_out($url)
-	{
-		if(substr($url, 0, 1) == "/")
-			return Site::get_url('user') . $url;
-		else
-			return $url;
-	}
-	
-	public function filter_post_type_display($type, $foruse) 
-	{ 
-	  $names = array( 
-		'image' => array(
-		  'singular' => _t('Image'),
-		  'plural' => _t('Images'),
-		)
-	  ); 
-	  return isset($names[$type][$foruse]) ? $names[$type][$foruse] : $type; 
-	}
-	
-	public function filter_template_user_filters($filters) {
-	  if ( isset($filters['content_type']) )
-	  {
-		$filters['content_type'] = Utils::single_array( $filters['content_type'] );
-		$filters['content_type'][] = Post::type('image');
-	  }
-	  return $filters;
-	}
 }
 ?>
