4b972af66dc42af57f4ffb5b92ed4b99442a34a2
Konzertheld <christian.gredig@gmail.com>
2013-03-22 22:35:45 +0100

    Change frm our no longer existing content type 'image' to 'photo'

--- multiple.image.php
+++
@@ -1,3 +0,0 @@
-<div class="post image">
-	<a href="<?php echo $content->info->fullsize_url; ?>" class="thickbox" title="<?php echo $content->title; ?>"><img src="<?php echo $content->info->thumbnail_url; ?>" class="contentpreview" alt="<?php echo $content->content; ?>"></a>
-</div>
