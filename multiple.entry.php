97c96c4f0f0e2af6ff2c0ff53279c2a02bc69353
Konzertheld <christian.gredig@gmail.com>
2013-03-24 20:12:02 +0100

    Improved vertical align for entry multiple view

--- multiple.entry.php
+++ multiple.entry.php
@@ -1,6 +1,6 @@
 <div class="post entry">
 	<div class="contentpreview">
 		<h2><a href="<?php echo $content->permalink; ?>" title="<?php echo $content->title; ?>"><?php echo $content->title_out; ?></a></h2>
-		<p><?php echo $content->content_excerpt; ?></p>
+		<div class="contentpreview_center"><p><?php echo $content->content_excerpt; ?></p></div>
 	</div>
 </div>
