3c2d2859f539da4a50ac93c87f17e766ba6f4c39
Konzertheld <christian.gredig@gmail.com>
2013-03-24 20:11:37 +0100

    Mutliple template for photosets

---
+++ multiple.photoset.php
@@ -0,0 +1,6 @@
+<div class="post photoset">
+	<div class="contentpreview">
+		<h2><a href="<?php echo $content->permalink; ?>" title="<?php echo $content->title; ?>"><?php echo $content->title_out; ?></a></h2>
+		<?php echo $content->content_singlesetphoto; ?>
+	</div>
+</div>
