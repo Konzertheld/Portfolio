7f17fa97411d3a8766e74f109ab5ea4631bd530e
Konzertheld <github@konzertheld.de>
2013-05-14 00:07:20 +0200

    Ignore user.css if it does not exist

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 	<meta name="generator" content="Habari <?php echo Version::get_habariversion() ?>" />
 	<link rel="stylesheet" type="text/css" href="<?php Site::out_url( 'theme' ); ?>/style.css" media="screen" />
-	<link rel="stylesheet" type="text/css" href="<?php Site::out_url( 'theme' ); ?>/user.css" media="screen" />
+	<?php if(is_file(Site::get_dir( 'theme' ) . '/user.css')): ?><link rel="stylesheet" type="text/css" href="<?php Site::out_url( 'theme' ); ?>/user.css" media="screen" /><?php endif; ?>
 	<?php echo $theme->header(); ?>
 </head>
 <body>
