0430bdb16fdba19f4354e8fb84ddc43937d1de4c
Konzertheld <github@konzertheld.de>
2013-09-09 03:00:04 +0200

    Add support for single photo content type

---
+++ multiple.photo.php
@@ -0,0 +1,5 @@
+<div class="post photo">
+	<div class="contentpreview">
+		<a href="<?php echo $content->singlephoto; ?>" title="<?php echo $content->title; ?>"><img src="<?php echo $content->singlephoto; ?>" alt="<?php echo $content->title; ?>"></a>
+	</div>
+</div>
