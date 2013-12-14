fb70acd0a7fb5a559fe8b153e334ff9f9f6b7678
Konzertheld <christian.gredig@gmail.com>
2013-12-14 20:21:55 +0100

    Add user.css and meta for responsive design

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 	<meta name="generator" content="Habari <?php echo Version::get_habariversion() ?>" />
 	<link rel="stylesheet" type="text/css" href="<?php Site::out_url( 'theme' ); ?>/style.css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="<?php Site::out_url( 'theme' ); ?>/user.css" media="screen" />
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<?php if(is_file(Site::get_dir( 'theme' ) . '/user.css')): ?><link rel="stylesheet" type="text/css" href="<?php Site::out_url( 'theme' ); ?>/user.css" media="screen" /><?php endif; ?>
 	<?php echo $theme->header(); ?>
 </head>
