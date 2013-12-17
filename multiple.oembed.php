ed8629ad91930fa341175df9a931606bcb2ae317
Konzertheld <christian.gredig@gmail.com>
2013-12-17 23:56:23 +0100

    Styling for oembed content

---
+++ multiple.oembed.php
@@ -0,0 +1,5 @@
+<div class="post oembed">
+	<div class="contentpreview">
+		<div class="contentpreview_center"><div class="contentpreview_content"><?php echo ($content->first) ? $content->content : $content->content_excerpt; ?></div></div>
+	</div>
+</div>
